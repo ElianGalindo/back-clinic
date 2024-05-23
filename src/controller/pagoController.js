390eeeb4be4e3c90f6117a90e8d0368bed2bbdd3
ElianGalindo <elianalejandro202@gmail.com>
2024-05-22 19:52:39 -0600

    arreglos

--- src/controller/pagoController.js
+++ src/controller/pagoController.js
@@ -2,28 +2,29 @@
 const stripe = require('stripe')('sk_test_51PHDADRxhro7mv4NGXI1LrJQmViXX6I711hrFJa2NSmZC1qpcrI5SlI66RCyPEmNVNRxmXbnHZ6ds8bh0qpTR5z800GpMe203J');
 
 const createCheckoutSession = async (req, res) => {
-  const { carrito } = req.body;
+  const { cartItems } = req.body;
 
   try {
+    const lineItems = cartItems.map(item => ({
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: item.nombre,
+        },
+        unit_amount: item.precio * 100, // Los montos en Stripe están en centavos
+      },
+      quantity: item.cantidad,
+    }));
+  
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
-      line_items: carrito.map(item => ({
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: item.nombre,
-            description: item.descripcion
-          },
-          unit_amount: parseFloat(item.precio) * 100
-        },
-        quantity: item.cantidad
-      })),
+      line_items: lineItems,
       mode: 'payment',
-      success_url: 'https://buy.stripe.com/test_4gwcPOdIX2hbfqU4gg',
-      cancel_url: 'http://localhost:3000/dashboard/Orders'
+      success_url: 'http://localhost:3000/pagos/exitoso',
+      cancel_url: 'http://localhost:3000/pagos/error',
     });
-
-    res.json({ sessionId: session.id });
+  
+    res.json({ id: session.id })
   } catch (error) {
     console.error('Error al crear sesión de pago:', error);
     res.status(500).json({ error: 'Error al procesar el pago' });
